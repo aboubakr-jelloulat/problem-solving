7ca6b9c0c2d155345a8a319f265e2152bc7d7615
Aboubakr Jelloulat <ajelloul@c1r7p7.1337.ma>
2025-01-22 16:01:12 +0100

    ..

---
+++ Codeforces/ICPC Assiut University Community/Sheet#1/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    string  name ;
+
+    getline (cin, name);
+    cout << "hello, " << name << endl;
+    
+    return (0);
+}
