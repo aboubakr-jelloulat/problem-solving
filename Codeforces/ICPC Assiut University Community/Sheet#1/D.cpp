f6cf94850db73376532751895f2204b4a923a597
Aboubakr Jelloulat <ajelloul@c1r6p8.1337.ma>
2025-01-23 10:35:17 +0100

    Diffrence

---
+++ Codeforces/ICPC Assiut University Community/Sheet#1/D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	long long	A, B, C, D;
+
+	cin >> A >> B >> C >> D;
+
+	cout << "Difference = " << (A * B) - (C * D) << endl;
+
+	return (0);
+}
