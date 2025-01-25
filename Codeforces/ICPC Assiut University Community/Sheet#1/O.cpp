c2367b7f588dad16912b518406ab3de493ed38cc
Aboubakr Jelloulat <ajelloul@c1r6p8.1337.ma>
2025-01-25 16:29:51 +0100

    Calculator

---
+++ Codeforces/ICPC Assiut University Community/Sheet#1/O.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int A, B;
+	char	op;
+	cin >> A >> op >> B;
+
+	if (op == '+')
+		cout << A + B;
+	if (op == '-')
+		cout << A  - B;
+	if (op == '*')
+		cout << A * B;
+	if (op == '/')
+		cout << A / B;
+
+	return (0);
+}
