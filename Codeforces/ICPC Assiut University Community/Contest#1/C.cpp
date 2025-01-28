4a402037cde2051e1bdf0570edeaad6c87f8043b
Aboubakr Jelloulat <ajelloul@c1r6p8.1337.ma>
2025-01-28 13:39:27 +0100

    Next Alphabet

---
+++ Codeforces/ICPC Assiut University Community/Contest#1/C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	char	c, x; cin >> c;
+
+	if (c == 'z')
+		cout << 'a';
+	else
+	{
+		x = c + 1;
+		cout << x ;
+	}
+		
+
+	return (0);
+}
