776c68deef84545840be0b6260b3d5a5de8ab062
Aboubakr Jelloulat <ajelloul@c1r6p8.1337.ma>
2025-02-02 12:26:10 +0100

    Pyramid

---
+++ Codeforces/ICPC Assiut University Community/Sheet#2/O.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	short N; cin >> N;
+
+	for (short i = 1; i <= N; i++)
+	{
+		for (short j = 1; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
