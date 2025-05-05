935822b08e58e26efec90ada60913bcdc60e46e6
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-05 15:55:29 +0100

    A. Alphabetic Removals

---
+++ Training/Level 1/Week 1/A.AlphabeticRemovals.cpp
@@ -0,0 +1,101 @@
+/**
+ * CP-Templates - Comprehensive Competitive Programming Template
+ * Author: [Aboubakr jelloulat]
+ * GitHub: [aboubakr-jelloulat]
+ * 
+ * Usage: Include this file at the beginning of your solution
+ * Platforms: LeetCode, Codeforces, and more ...
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// <bits/stdc++.h>    ********** not available on macOS by default ***********
+
+using namespace std;
+
+// Type definitions
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<ll> > vvll;
+typedef vector<vector<int> > vvi;
+
+
+typedef  vector <string> vs;
+
+// Loops and iteration
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+// Useful functions
+
+void	NITRO() {ios::sync_with_stdio(0); cin.tie(0);}
+
+
+
+
+void	AlphabeticRemovals(string &s, int k, int len)
+{
+
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		for (int u = 0; u < len && k != 0; u++)
+		{
+			if (i == s[u])
+			{
+				s.erase(u, 1);
+				len --;
+				k--;
+				u--; // not skip the next character
+				//s[u] == NULL;
+			}
+		}
+		if (!k)
+			break;
+	}
+}
+
+
+int32_t	main(void)
+{
+	NITRO();
+
+	int	len, k; cin >> len >> k;
+	string s; cin >> s;
+
+	AlphabeticRemovals(s, k, len);
+
+	cout << s << endl;
+	
+	// printf using loop
+	//if s[i] != NULL; cout << s[i];
+
+	return 0;
+}
