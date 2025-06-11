26431d23ff7ea311fceb3a60039c0e583dba3661
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-11 18:38:31 +0100

    543 Diameter of Binary Tree

---
+++ LeetCode/Binary Tree/543.DiameterofBinaryTree.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int max_sofar = 0;
+
+int height(struct TreeNode* root)
+{
+    if (!root) return 0;
+
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+
+    max_sofar = MAX(max_sofar, right_height + left_height);
+
+    return (1 + MAX(right_height, left_height));
+}
+
+int diameterOfBinaryTree(struct TreeNode* root)
+{
+    if (!root) return 0;
+
+    max_sofar = 0;
+    height(root);
+    return max_sofar;
+}
