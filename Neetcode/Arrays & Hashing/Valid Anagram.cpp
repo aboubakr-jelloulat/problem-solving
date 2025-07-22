1c22b5f1ce3e7bf3e44a05a6b01213e8111dbf24
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-22 13:27:00 +0100

    Valid Anagram  242 leetcode

--- Neetcode/Arrays & Hashing/Valid Anagram.cpp
+++ Neetcode/Arrays & Hashing/Valid Anagram.cpp
@@ -40,19 +40,45 @@ public:
 //   ************  2. Hash Map   ************  
 
 
+/*
+    Time & Space Complexity :
+
+        Time complexity:  O(n+m)
+        Space complexity: O(1)
+
+*/
+
 class Solution 
 {
 public:
     bool isAnagram(string s, string t) 
     {
+        if (s.length() != t.length())
+            return false;
+
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int nb : count)
+            if (nb)
+                return false;
         
+        return true;
     }
 };
 
 
-
 int main(int ac, char **av)
 {
+    if (ac < 3) {
+        cout << "Usage:  <string1> <string2>" << endl;
+        return 1;
+    }
     Solution answer;
 
     (answer.isAnagram(av[1], av[2])) ? cout << "true" : cout << "false";
