98efdd9ac92afedc1d1e1fc4b4875b423a525094
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-09-06 20:05:14 +0100

    Rising Temperature

--- sql-50-study-plan/197.sql
+++ sql-50-study-plan/197.sql
@@ -1,8 +1,8 @@
 
--- Topic : Self join
+-- Topic : Self join +++ 
 
 
 SELECT W1.id FROM Weather W1  JOIN Weather W2
 
 ON W2.recordDate = DATEADD(day, -1, W1.recordDate)
-WHERE W1.temperature > W2.temperature;
+WHERE W1.temperature > W2.temperature; 
