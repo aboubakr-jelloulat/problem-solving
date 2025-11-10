88b569277b8c8238edc2865489132899a399437c
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-11-10 13:06:21 +0100

    c

---
+++ Grokking/preparation-level/sheet1/C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+void	foo()
+{
+	int n, q; cin >> n >> q;
+
+	ll arr[n];
+	for (ll i = 0; i < n; i++)
+		cin >> arr[i];
+
+	while (q--)
+	{
+		int l, r, k;  cin >> l >> r >> k;
+		
+	}
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+	int t;  cin >> t;
+	while(t--)
+		foo();
+
+	return (0);
+}
