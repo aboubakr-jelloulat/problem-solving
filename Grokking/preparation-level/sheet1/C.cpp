a20c1425324293ac4d72cba30ae4ed2df5153d19
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-11-10 19:19:49 +0100

    C. Odd Queries

--- Grokking/preparation-level/sheet1/C.cpp
+++ Grokking/preparation-level/sheet1/C.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-typedef long long ll;
+using ll = long long;
+using vl = vector<ll>;
 
-void	foo()
+void foo()
 {
-	int n, q; cin >> n >> q;
+	int n, q;
+	cin >> n >> q;
 
-	ll arr[n];
+	vl a(n);
+	vl prfx(a.size() + 1, 0); // init with 0 Gv
+
 	for (ll i = 0; i < n; i++)
-		cin >> arr[i];
+		cin >> a[i];
+
+	for (ll i = 0; i < n; ++i)
+		prfx[i + 1] = prfx[i] + a[i];
 
 	while (q--)
 	{
-		int l, r, k;  cin >> l >> r >> k;
-		
-	}
+		int l, r, k;
+		cin >> l >> r >> k;
 
+		ll diff = prfx[r] - prfx[l - 1];
+		ll rust = prfx[n] - diff;
+		rust += (k * (r - l + 1));
+
+		cout << ((rust % 2 == 0) ? "NO" : "YES") << endl;
+	}
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	cin.tie(nullptr);
 
-	int t;  cin >> t;
-	while(t--)
+	int t;
+	cin >> t;
+	while (t--)
 		foo();
 
 	return (0);
