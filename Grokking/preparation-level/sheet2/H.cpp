7e8bc5af4a4ba527dc88d005d11b7238b3a47b78
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-11-15 14:21:33 +0100

    H. Interesting drink

--- Grokking/preparation-level/sheet2/H.cpp
+++ Grokking/preparation-level/sheet2/H.cpp
@@ -3,31 +3,23 @@
 #include <algorithm>
 using namespace std;
 
-
-
 int main()
 {
     int n;  cin >> n;
     
-	vector<int> arr(n);
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-	sort(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end());
 
-	int q;  cin >> q;
-	while (q--)
-	{
-		int day;  cin >> day;
+    int q;  cin >> q;
+    while (q--)
+    {
+        int day;  cin >> day;
 
-		if (day < arr[0])
-		{
-			cout << 0 << endl;
-			continue;
-		}
-		auto it = std::lower_bound(arr.begin(), arr.end(), day);
-		int index = it - arr.begin() + 1;
-		cout << index << endl;
-	}
-
+        auto it = upper_bound(arr.begin(), arr.end(), day);
+        int index = it - arr.begin();
+        cout << index << endl;
+    }
 }
