a7c0b0597dce589abdad616ee43302090f3d1bae
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-11-20 16:01:40 +0100

    Hej

---
+++ Codeforces/Div/Div3/Codeforces Round 1065 (Div. 3)/A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main(void)
+{
+	int t;  cin >> t;
+
+	while (t--)
+	{	
+
+		int n;  cin >> n;
+	
+		if (n % 2 == 0)
+		{
+			cout << 0 << endl;
+			continue;
+		}
+
+		int off = n / 2;
+		int 
+
+		cout << off << endl;
+
+	}
+	
+
+	return 0;
+}
