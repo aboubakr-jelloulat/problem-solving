afb1a158ad17f73b8ba6fd560d463997ba09f99b
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-02-01 13:57:08 +0100

    643. Maximum Average Subarray I

---
+++ 75 Leetcode/Sliding Window/643.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    double findMaxAverage(vector<int>& nums, int k) 
+    {
+        long long sub_sum = 0;
+        long long max_sum = 0;
+
+       
+        for (int i = 0; i < k; i++)
+            sub_sum += nums[i];
+
+        max_sum = sub_sum;
+
+       
+        for (int i = k; i < nums.size(); i++)
+        {
+            sub_sum += nums[i]; 
+            sub_sum -= nums[i - k];
+
+            max_sum = std::max(max_sum, sub_sum);
+        }
+
+        
+        return ((double)max_sum / k);
+    }
+};
