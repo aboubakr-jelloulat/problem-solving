45f5f9fe4750f9df533351e2513e057aaf3fc1f3
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-02-25 22:13:04 +0000

    328. Odd Even Linked List

---
+++ 75 Leetcode/Linked List/328.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution 
+{
+public:
+
+    static ListNode* oddEvenList(ListNode* head) 
+    {
+        if (!head || !head->next)
+            return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even && even->next)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+
+        return head;
+    }
+};
+
+int main()
+{
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    head = Solution::oddEvenList(head);
+
+
+    while (head)
+    {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+
+    return 0;
+}
+
